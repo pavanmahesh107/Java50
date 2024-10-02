b76bef405ea859a60d70ab6489aad675d47129b6
pavanmahesh1518 <pavanmahesh107@gmail.com>
2024-10-02 11:04:23 +0530

    System Class

---
+++ src/JavaInterviewPrep/Systemerr.java
@@ -0,0 +1,7 @@
+package JavaInterviewPrep;
+
+public class Systemerr {
+    public static void main(String[] args) {
+        System.err.println("Printing the Error Message");
+    }
+}
---
+++ src/JavaInterviewPrep/Systemin.java
@@ -0,0 +1,13 @@
+package JavaInterviewPrep;
+
+import java.util.Scanner;
+
+public class Systemin {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int X = scanner.nextInt();
+        int Y = scanner.nextInt();
+
+        System.err.println("Addition is Corrupted " +(X + Y));
+    }
+}
