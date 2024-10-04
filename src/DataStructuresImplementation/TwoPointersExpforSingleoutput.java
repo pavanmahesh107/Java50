10b1867ad9893b19e597c1663c1c530e3d0712c4
pavanmahesh1518 <pavanmahesh107@gmail.com>
2024-10-04 19:39:49 +0530

    Two pointer approaches

--- src/DataStructuresImplementation/TwoPointersExpforSingleoutput.java
+++ src/DataStructuresImplementation/TwoPointersExpforSingleoutput.java
@@ -1,29 +1,31 @@
 package DataStructuresImplementation;
 
-public class TwoPointersExp {
-    public static boolean findPairWithSum(int[] arr, int target) {
+// Two pointers for a single pair. If the array is Sorted.
+
+public class TwoPointersExpforSingleoutput {
+    public static void findPairWithSum(int[] arr, int target) {
         int left = 0; // Start pointer
         int right = arr.length - 1; // End pointer
 
         while (left < right) {
             int sum = arr[left] + arr[right];
 
+            // If the sum is equals to the target, return true.
             if (sum == target) {
                 System.out.println("Pair found: (" + arr[left] + "," + arr[right]+")");
-                return true;
+                return;
             }
             if (sum < target) {
-                left++;
+                left++;   // If the sum is lesser that the target, increment left to increase the sum.
             } else {
-                right--;
+                right--; // If the sum is greater than the target, decrement right to decrease the sum.
             }
         }
         System.out.println("No pair found with the given target sum");
-        return false;
     }
 
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 6, 8, 11};
+        int[] arr = {1, 2, 3, 4, 6, 8, 11};  // Sorted array
         int target = 10;
         findPairWithSum(arr, target);
     }
---
+++ src/DataStructuresImplementation/TwoPointersforManyoutput.java
@@ -0,0 +1,47 @@
+package DataStructuresImplementation;
+
+// Two pointers sum for a multiple pairs. If the array is Sorted.
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TwoPointersforManyoutput {
+    public static List<int[]> findPairs(int[] arr, int target){
+        List<int[]> pairs = new ArrayList<>();
+        int left = 0;
+        int right = arr.length -1;
+
+        while(left < right) {
+            int sum = arr[left] + arr[right];
+
+            if (sum == target) {
+                pairs.add(new int[]{arr[left], arr[right]});
+                left++;
+                right--;
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return pairs;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 4, 5, 6, 7};
+        int target = 7;
+
+        System.out.println("Array:");
+        printArray(arr);
+        System.out.println("\nPairs with sum " + target + ":");
+        List<int[]> pairs = findPairs(arr, target);
+        for (int[] pair : pairs) {
+            System.out.println(pair[0] + ", " + pair[1]);
+        }
+    }
+        private static void printArray(int[] arr){
+            for(int num : arr){
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+}
