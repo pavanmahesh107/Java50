92e1b70c27a73589f386c8c8cd19e74adb7bcb0e
pavanmahesh1518 <pavanmahesh107@gmail.com>
2024-10-07 13:16:29 +0530

    Two Pointers Hashtable

---
+++ src/DataStructuresImplementation/TwoPointers/TwoPointersHashTable.java
@@ -0,0 +1,34 @@
+package DataStructuresImplementation.TwoPointers;
+
+import java.util.HashMap;
+
+public class TwoPointersHashTable {
+    public static int[] findPairs(int[] arr, int target) {
+        if (arr == null || arr.length < 2) {
+            return new int[]{};
+        }
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (hashMap.containsKey(target - arr[i])) {
+                return new int[]{hashMap.get(target - arr[i]), i};
+            }
+            hashMap.put(arr[i], i);
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 2, 4};
+        int target = 6;
+        int[] res = findPairs(arr, target);
+        printArray(res, target);
+    }
+
+    private static void printArray(int[] arrResult, int target) {
+        if (arrResult.length == 2) {
+            System.out.println("Pair found for target " + target + " at indices " + arrResult[0] + " and " + arrResult[1]);
+        } else {
+            System.out.println("No target found");
+        }
+    }
+}
