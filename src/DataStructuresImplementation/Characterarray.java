9d33828ce8c8847b45151f36804c130c3d7a67a4
pavanmahesh1518 <pavanmahesh107@gmail.com>
2024-10-08 08:45:44 +0530

    Examples

---
+++ src/DataStructuresImplementation/Characterarray.java
@@ -0,0 +1,12 @@
+package DataStructuresImplementation;
+
+import java.util.Arrays;
+
+public class Characterarray {
+    public static void main(String[] args) {
+        String myString = "Hello, World";
+        char[] myChar = myString.toCharArray();  // toCharArray -> Converts a String into Array of Characters.
+
+        System.out.println(Arrays.toString(myChar));  //Arrays.toString -> Converts an Array into Human readable String.
+    }
+}
