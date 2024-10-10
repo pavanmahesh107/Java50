af65cb04a9f9f336086780bbf09cdfbf735318b8
pavanmahesh1518 <pavanmahesh107@gmail.com>
2024-10-10 12:54:23 +0530

    Experimanttaion Package in using Inbuilt

--- src/DataStructuresImplementation/Experimentations/SortZeroOne.java
+++ src/DataStructuresImplementation/Experimentations/SortZeroOne.java
@@ -1,28 +1,19 @@
 package DataStructuresImplementation.Experimentations;
 
+import java.util.Arrays;
+
 public class SortZeroOne {
     public static int[] sortZeOn(int[] arr) {
-
-        int n = arr.length;
-        int countZero = 0;
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == 0) {
-                arr[countZero] = 0 ;
-                countZero++;
+        Arrays.sort(arr);
+      return arr;
+    }
+        public static void main (String[]args){
+            int[] arr = {0, 1, 0, 0, 1, 1, 0, 1, 0};
+            int[] res = sortZeOn(arr);
+            for(int num : res) {
+                System.out.print(num);
             }
+            System.out.println();
         }
-        for(int i = countZero; i < n; i++){
-            arr[i] = 1;
-        }
-        return arr;
-    }
-
-    public static void main(String[] args) {
-        int[] arr = {0, 1, 0, 0, 1, 1, 0, 1, 0};
-        int[] res = sortZeOn(arr);
-        for (int num : res) {
-            System.out.print(num);
-        }
-        System.out.println();
-    }
 }
+// The Time Complexity for inbuilt Arrays.sort(arr) is O(nlogn) which is not recommended.
