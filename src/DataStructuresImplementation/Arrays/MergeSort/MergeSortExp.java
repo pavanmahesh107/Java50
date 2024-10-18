8bc63b290d65115da995f9e361ddd92cd8ba465f
pavanmahesh1518 <pavanmahesh107@gmail.com>
2024-10-18 13:55:44 +0530

    Merge Sort

---
+++ src/DataStructuresImplementation/Arrays/MergeSort/MergeSortExp.java
@@ -0,0 +1,28 @@
+package DataStructuresImplementation.Arrays.MergeSort;
+
+public class MergeSortExp {
+    public static void mergeSort(int[] arr, int left, int mid, int right){
+        // Find sizes of two subarrays to be merged
+        int n1 = mid - left + 1;
+        int n2 = right -mid;
+
+        // Create temp arrays
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        // Copy data to temp arrays
+        for(int i = 0; i < n1; i++){
+            leftArray[i] = arr[left + i];
+        }
+        for(int j = 0; j < n2; j++){
+            rightArray[j] = arr[mid + 1 + j];
+        }
+
+        //
+
+
+    }
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6, 7};
+    }
+}
