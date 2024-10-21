9412db42115c8b869602bf4924b40afa24304779
pavanmahesh1518 <pavanmahesh107@gmail.com>
2024-10-21 09:53:34 +0530

    Binary Search Implementation using Iterative

---
+++ src/DataStructuresImplementation/Arrays/Searching/BinarySearchingIterative.java
@@ -0,0 +1,41 @@
+package DataStructuresImplementation.Arrays.Searching;
+
+public class BinarySearchingIterative {
+
+    // Iterative implementation of binary search.
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // Check if the target is present in the mid.
+            if (arr[mid] == target) {
+                return mid; // Target found.
+            }
+
+            // If the target is greater, ignore the left half.
+            if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                // If the target is lesser, ignore the right half.
+                right = mid - 1;
+            }
+        }
+
+        return -1; // Target not found.
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 7, 11, 13, 17};
+        int target = 11;
+
+        int res = binarySearch(arr, target);
+        if (res == -1) {
+            System.out.println("Target not  found");
+        } else {
+            System.out.println(" Target found at index : " + res);
+        }
+    }
+}
