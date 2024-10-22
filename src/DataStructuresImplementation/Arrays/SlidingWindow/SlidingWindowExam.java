248222417978c1af508a1d058b4785ec92f3c09f
pavanmahesh1518 <pavanmahesh107@gmail.com>
2024-10-22 17:56:57 +0530

    Sliding Window - Maximum sum of K consecutive elememnts with T.C & S.C

--- src/DataStructuresImplementation/Arrays/SlidingWindow/SlidingWindowExam.java
+++ src/DataStructuresImplementation/Arrays/SlidingWindow/SlidingWindowExam.java
@@ -11,12 +11,15 @@ public class SlidingWindowExam {
         }
 
         // Caluclate the sum of the first 'k' elements(initial window).
+        // T.C = O(k) for the first k elements.
         int maxSum = 0;
         for (int i = 0; i < k; i++) {
             maxSum += arr[i];
         }
 
-        // Use the sliding window technique to find the maxSum
+        // Use the sliding window technique to find the maxSum.
+        // T.C = O(n-k) ~ O(n); For the remaining elements,
+        // we perform n - k operations (one iteration per element), each taking constant time O(1).
         int windowSum = maxSum;
         for (int i = k; i < arr.length; i++) {
             windowSum += arr[i] - arr[i - k];
@@ -33,3 +36,7 @@ public class SlidingWindowExam {
         System.out.println("The maximum sum of " + k + " consecutive element is: " + res);
     }
 }
+
+// Overall T.C is O(k) + O(n-k) = O(n)
+// Time complexity - O(n)
+// Space complexity - O(1)
