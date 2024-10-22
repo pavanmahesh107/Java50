ca5d6f43d9404bd63e88d4242ddcd1ecadd88666
pavanmahesh1518 <pavanmahesh107@gmail.com>
2024-10-22 12:59:50 +0530

    Sliding Window - Maximum sum of K consecutive elememnts

---
+++ src/DataStructuresImplementation/Arrays/SlidingWindow/SlidingWindowExam.java
@@ -0,0 +1,14 @@
+package DataStructuresImplementation.Arrays.SlidingWindow;
+
+
+// Find a maximum sum of K consecutive elements
+public class SlidingWindowExam {
+    public static int maxSum(int[] arr, int k){
+
+        return 0;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        int k = 3;
+    }
+}
