31be0ad47c41610e5f5b7b29ac3cd008a8d6f9bc
pavanmahesh1518 <pavanmahesh107@gmail.com>
2024-10-24 19:16:21 +0530

    Linked List Traversal

--- src/DataStructuresImplementation/LinkedListsimp/LinkedListTraversal.java
+++ src/DataStructuresImplementation/LinkedListsimp/LinkedListTraversal.java
@@ -1,17 +1,24 @@
 package DataStructuresImplementation.LinkedListsimp;
+// Node structure
+class Node {
+    int data;  // value stored in the node.
+    Node next;  // Reference to the next node.
+
+    Node(int data) {
+        this.data = data;
+        this.next = null; // Next is initially null.
+    }
+}
 
 public class LinkedListTraversal {
+     Node head;
 
-    // Node structure
-
-    static class Node {
-        int data;  // value stored in the node.
-        Node next;  // Reference to the next node.
-
-        Node(int data) {
-            this.data = data;
-            this.next = null; // Next is initially null.
-        }
-
-    }
+     public void traverse(){
+         Node current = head;
+         while( current != null){
+             System.out.println(current.data + " -> ");
+             current = current.next;
+         }
+         System.out.println("null");
+     }
 }
