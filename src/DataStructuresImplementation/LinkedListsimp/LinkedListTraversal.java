4644a1a07a7686c0e62604bc25698edb69697ad3
pavanmahesh1518 <pavanmahesh107@gmail.com>
2024-10-24 19:03:59 +0530

    Linked List Node Creation

---
+++ src/DataStructuresImplementation/LinkedListsimp/LinkedListTraversal.java
@@ -0,0 +1,17 @@
+package DataStructuresImplementation.LinkedListsimp;
+
+public class LinkedListTraversal {
+
+    // Node structure
+
+    static class Node {
+        int data;  // value stored in the node.
+        Node next;  // Reference to the next node.
+
+        Node(int data) {
+            this.data = data;
+            this.next = null; // Next is initially null.
+        }
+
+    }
+}
